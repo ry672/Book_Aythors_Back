d77bb61beb001ae41ccb111b2336d3df255aa963
ry672 <Rufina_Garaeva@student.itpu.uz>
2026-03-11 04:23:35 +0500

    final14

--- src/category/model/category.model.ts
+++ src/category/model/category.model.ts
@@ -12,7 +12,7 @@ export class CategoryModel extends Model<CategoryModel, CategoryCreate> {
   @Column({ type: DataType.INTEGER, autoIncrement: true, primaryKey: true })
   declare id: number;
 
-  @Column({ type: DataType.STRING, allowNull: false, unique: true })
+  @Column({ type: DataType.STRING, allowNull: false})
   declare name: string;
 
   @Column({ type: DataType.STRING, allowNull: false, defaultValue: 'active' })
