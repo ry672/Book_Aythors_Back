04947bd713b25f145a0866cb4712dc37a931f56f
ry672 <Rufina_Garaeva@student.itpu.uz>
2026-03-10 15:42:07 +0500

    final10

--- src/imagekit/imagekit.service.ts
+++ src/imagekit/imagekit.service.ts
@@ -1,5 +1,4 @@
-
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import ImageKit from 'imagekit';
 
 @Injectable()
@@ -7,12 +6,17 @@ export class ImagekitService {
   private readonly imagekit: ImageKit;
 
   constructor() {
+    console.log('IMAGEKIT_PUBLIC_KEY exists:', !!process.env.IMAGEKIT_PUBLIC_KEY);
+    console.log('IMAGEKIT_PRIVATE_KEY exists:', !!process.env.IMAGEKIT_PRIVATE_KEY);
+    console.log('IMAGEKIT_URL_ENDPOINT exists:', !!process.env.IMAGEKIT_URL_ENDPOINT);
+    console.log('IMAGEKIT_URL_ENDPOINT value:', process.env.IMAGEKIT_URL_ENDPOINT);
+
     const publicKey = process.env.IMAGEKIT_PUBLIC_KEY;
     const privateKey = process.env.IMAGEKIT_PRIVATE_KEY;
     const urlEndpoint = process.env.IMAGEKIT_URL_ENDPOINT;
 
     if (!publicKey || !privateKey || !urlEndpoint) {
-      throw new InternalServerErrorException('ImageKit env variables are missing');
+      throw new Error('ImageKit env variables are missing');
     }
 
     this.imagekit = new ImageKit({
