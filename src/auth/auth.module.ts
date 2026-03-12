f4b581403c8576393fcb5acc604f462aaebfcc6d
ry672 <Rufina_Garaeva@student.itpu.uz>
2026-03-12 12:27:36 +0500

    final45

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { AuthorModule } from 'src/author/author.module';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { AuthorModel } from 'src/author/model/author.model';
 
 @Module({
   imports: [
     ConfigModule,
-    SequelizeModule.forFeature([AuthorModule]),
+    SequelizeModule.forFeature([AuthorModel]),
     JwtModule.register({})
   ],
   controllers: [AuthController],
