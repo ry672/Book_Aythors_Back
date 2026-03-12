638b0cfd31d18fc97d6557925220670b70d1e134
ry672 <Rufina_Garaeva@student.itpu.uz>
2026-03-12 12:14:25 +0500

    final42

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -4,10 +4,10 @@ import { AuthorService } from './author.service';
 import { AuthorController } from './author.controller';
 import { AuthorModel } from './model/author.model';
 import { BookModel } from 'src/books/model/books.model';
-import { AuthModule } from 'src/auth/auth.module';
+
 
 @Module({
-  imports: [SequelizeModule.forFeature([AuthorModel, BookModel, AuthModule])],
+  imports: [SequelizeModule.forFeature([AuthorModel, BookModel])],
   providers: [AuthorService],
   controllers: [AuthorController],
   exports: [AuthorService],
