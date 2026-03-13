de08fb0b4f7b9a9ddffe36ae3682116d231921af
ry672 <Rufina_Garaeva@student.itpu.uz>
2026-03-13 06:51:20 +0500

    final58

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { AuthModule } from './auth/auth.module';
       database: process.env.DB_NAME,
       autoLoadModels: true,
       synchronize: true,
-      sync: { force: true },
+      sync: { force: false },
       logging: false,
     }),
     AuthorModule,
